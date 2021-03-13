eed02e880d0e6cd720d1ed0bb713ee53dc0221e1
nabdeepkour <77386987+nabdeepkour@users.noreply.github.com>
2021-03-13 14:23:51 +0530

    Add files via upload

---
+++ sand.js
@@ -0,0 +1,50 @@
+class Sand{
+
+    constructor(x,y,r){
+        var options={
+         restitutions:0.3,
+         friction:5,
+         density:1 
+        }
+        this.x=x;
+         this.y=y;
+         this.r=r
+         this.body=Bodies.circle(this.x,this.y(this.r-20)/2,options)
+        World.add(world,this.body);
+    }
+
+   display(){
+   var sandpos= this.body.positions;
+ push()
+ translate(sandpos.x,sandpos.y);
+ rectMode(CENTRE)
+ strokeWeight(4);
+ stroke("black");
+ fill("red");
+   ellipse(0,0,this,r,this,r);
+   pop();
+
+
+
+
+
+
+   }
+   
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+}
